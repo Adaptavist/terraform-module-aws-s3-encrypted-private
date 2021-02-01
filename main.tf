4d89054d8c4706fb96b392eadeb43c96f6417924
Lilly Holden <lholden@adaptavist.com>
2021-02-01 10:08:01 +0000

    fix: added missing param

--- main.tf
+++ main.tf
@@ -22,14 +22,15 @@ module "kms" {
 }
 
 module "s3" {
-  source                    = "./modules/s3"
-  namespace                 = var.namespace
-  stage                     = var.stage
-  name                      = var.name
-  tags                      = var.tags
-  bucket_name               = var.bucket_suffix
-  kms_key_arn               = module.kms.key_arn
-  use_bucket_suffix_as_name = var.use_bucket_suffix_as_name
+  source                                = "./modules/s3"
+  namespace                             = var.namespace
+  stage                                 = var.stage
+  name                                  = var.name
+  tags                                  = var.tags
+  bucket_name                           = var.bucket_suffix
+  kms_key_arn                           = module.kms.key_arn
+  use_bucket_suffix_as_name             = var.use_bucket_suffix_as_name
+  enforce_server_side_encryption_header = var.enforce_server_side_encryption_header
 }
 
 module "s3-iam-roles" {
