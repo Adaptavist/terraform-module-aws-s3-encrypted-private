29a4b578bb488149944571f384f501c33cd50779
Zbysek MRAZ <zmraz@adaptavist.com>
2022-03-07 15:21:58 +0100

    BREAKING CHANGE: server side encryption resources

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -14,15 +14,6 @@ resource "aws_s3_bucket" "this" {
   #checkov:skip=CKV_AWS_144:cross region replication not required
   bucket = var.use_bucket_suffix_as_name ? var.bucket_name : "${module.labels.id}-${var.bucket_name}"
   tags   = module.labels.tags
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = var.kms_key_arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
 }
 
 resource "aws_s3_bucket_acl" "this" {
@@ -34,6 +25,17 @@ resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
     status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryoption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.kms_key_arn
+      sse_algorithm     = "aws:kms"
+    }
   }
 }
 
