fc4f416070a3cdc970ab66a5898bb4d28cd94bc2
Levi Lawrence <llawrence@adaptavist.com>
2023-06-28 09:31:37 +0100

    set var.log_expiration_days default to 30

--- variables.tf
+++ variables.tf
@@ -84,5 +84,5 @@ variable "lifecycle_rule" {
 variable "log_expiration_days" {
   description = "The number of days after which the object in the bucket expires"
   type        = number
-  default     = null
+  default     = 30
 }
